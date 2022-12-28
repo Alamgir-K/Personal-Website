162909c9e20832f92c5c0d56f1bedc1638e39f38
Alamgir Khan <k.alamgir2002@gmail.com>
2022-12-27 23:15:18 -0500

    updated experience responsive styling

--- public/styleScript.js
+++ public/styleScript.js
@@ -26,42 +26,67 @@ let rightRevealElements = []
 function watchForReveal() {
     window.addEventListener('scroll', () => {
         let scrollFactor = 1.0
+        let leftStagger = 25
+        let rightStagger = 25
+        let leftMultipler = 1.0
+
+        if (window.innerWidth < 1024) {
+            scrollFactor = 0.20
+            leftMultipler = 3.0
+            leftStagger = 5
+            rightStagger = 5
+        }
+
+        if (window.innerWidth < 768) {
+            scrollFactor = 0.25
+            leftMultipler = 4.0
+            leftStagger = 5
+            rightStagger = 5
+        }
+
+        if (window.innerWidth < 648) {
+            scrollFactor = 0.25
+            leftMultipler = 8.0
+            leftStagger = 5
+            rightStagger = 5
+        }
+
         let scrolled = window.pageYOffset * 0.1
         let leftIndex = 0
-        let leftStagger = 25
-        let translate = scrollFactor * scrolled
-        const translateCopy = translate
+        let rightIndex = 0
+        
+        let leftTranslate = scrollFactor * scrolled * leftMultipler
+        let rightTranslate = scrollFactor * scrolled
 
         for (const leftRevealElement of leftRevealElements) {
             leftIndex++
             const currentLeft = window.scrollX + leftRevealElement.getBoundingClientRect().left
-            console.log(translate)
 
             if (leftIndex > 1) {
-                translate = scrollFactor * scrolled - leftStagger
+                leftTranslate = ((scrollFactor * scrolled) - leftStagger) * leftMultipler
                 leftStagger = leftStagger * 2
             }
 
             if (currentLeft > 10.0) {
-                leftRevealElement.style.transform = `translateX(-${translate}%)`
+                leftRevealElement.style.transform = `translateX(-${leftTranslate}%)`
             }
         }
-        
-        let rightIndex = 0
-        let rightStagger = 25
-        translate = translateCopy
 
         for (const rightRevealElement of rightRevealElements) {
             rightIndex++
             const currentRight = window.scrollX + rightRevealElement.getBoundingClientRect().right
 
             if (rightIndex > 1) {
-                translate = scrollFactor * scrolled - rightStagger
+                rightTranslate = scrollFactor * scrolled - rightStagger
                 rightStagger = rightStagger * 2
             }
 
             if (currentRight < window.innerWidth - 10.0) {
-                rightRevealElement.style.transform = `translateX(${translate}%)`
+                console.log('index' + rightIndex + ' ' + rightTranslate)
+                rightRevealElement.style.transform = `translateX(${rightTranslate}%)`
+            }
+            else {
+                // console.log('done' + rightIndex)
             }
         }
     })
@@ -88,7 +113,11 @@ async function scrollReveal() {
 
 // window.onbeforeunload = function () {
 //     window.scrollTo(0, 0);
-//   }
+// }
+
+window.onresize = function() { 
+    location.reload(); 
+    // window.scrollTo(0, 0);
+}
 
 scrollReveal()
-
--- src/components/ExperienceItem/ExperienceItem.js
+++ src/components/ExperienceItem/ExperienceItem.js
@@ -7,35 +7,32 @@ export default function ExperienceItem(props) {
   </p>)
 
   return (
-    <div className='flex w-full justify-center relative gap-2'>
+    <div className='flex w-full justify-center content-center relative gap-2'>
         
-        <div className='md:hidden lg:flex flex flex-wrap lg:absolute lg:inset-x-1/4 items-center justify-center content-center h-52 lg:h-96 xl:h-72 w-full lg:w-1/2 z-20 rounded-[70px] experience-title-block scroll-reveal reveal-right'>
+        <div className='flex flex-wrap absolute inset-x-1/8 lg:inset-x-1/4 items-center justify-center content-center h-52 md:h-72 lg:h-96 xl:h-72 w-3/4 lg:w-1/2 z-20 rounded-[70px] experience-title-block scroll-reveal reveal-right'>
               <div className='flex flex-row basis-full justify-center'>
               <p className={`text-center tx-base lg:text-lg font-normal text-black`}>{props.item.role}</p>
               </div>
-              {/* <div className='md:hidden flex flex-row basis-full justify-center'>
-                <img src={`images/${props.item.logo}`} className='max-w-[100px] max-h-[100px]'></img>
-              </div> */}
               <div className='flex flex-row basis-full justify-center'>
               <p className={`text-center text-3xl lg:text-5xl font-semibold text-black m-4`}>{props.item.company}</p>
               </div>
-            
         </div>
-        <div className='hidden md:flex lg:absolute lg:inset-x-1/4 justify-center md:h-72 lg:h-96 xl:h-72 w-full lg:w-1/2 z-10 rounded-[70px] experience-description-block scroll-reveal reveal-left'>
+
+        <div className='flex absolute inset-x-32 lg:inset-x-1/4 justify-center h-52 md:h-72 lg:h-96 xl:h-72 w-1/4 lg:w-1/2 z-10 rounded-[70px] experience-description-block scroll-reveal reveal-left'>
             <div className='grid grid-cols-4 w-full'>
-              <div className='flex justify-center items-center content-center col-span-1 row-span-4'>
+              <div className='flex justify-center items-center content-center col-span-4 lg:col-span-1 row-span-4'>
                 <img src={`images/${props.item.logo}`} className='max-w-[100px] max-h-[100px]'></img>
               </div>
-              <div className='flex flex-wrap place-self-start mt-8 basis-full col-span-3'>
+              <div className='hidden lg:flex flex-wrap place-self-start mt-8 basis-full col-span-3'>
                 <p className='font-semibold'>{props.item.startDate} - {props.item.endDate}</p>
               </div>
-              <div className='flex flex-wrap items-center col-span-3'>
+              <div className='hidden lg:flex flex-wrap items-center col-span-3'>
                 {points}
               </div>
             </div>
         </div>
 
-        <div className='hidden lg:flex absolute right-auto h-96 xl:h-72 w-1/2 z-1 experience-description-block'>
+        <div className='flex absolute inset-x-1/8 h-52 md:h-72 lg:h-96 xl:h-72 w-3/4 lg:w-1/2 rounded-[70px] z-1 experience-description-block'>
         </div>
         
     </div>
