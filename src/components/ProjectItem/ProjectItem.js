45fe4dd3940be2a846d4b5973db2d80c3804a20f
Alamgir Khan <k.alamgir2002@gmail.com>
2023-02-22 21:44:12 -0500

    fixed mobile view for projects section

--- src/components/ProjectItem/ProjectItem.js
+++ src/components/ProjectItem/ProjectItem.js
@@ -2,25 +2,44 @@ import React from 'react'
 
 export default function ProjectItem(props) {
   return (
-    <div className='flex flex-wrap h-96 w-full justify-between rounded-[70px]' style={{backgroundColor: props.item.bgColor, color: props.item.textColor ? props.item.textColor : 'black'}}>
+    <div className='flex flex-wrap h-96 w-full justify-between rounded-[60px]
+            lg:rounded-[70px]' 
+            style={{backgroundColor: props.item.bgColor, color: props.item.textColor ? props.item.textColor : 'black'}}>
         
         <div className='flex w-full justify-between px-10 pt-10'>
             <p className="text-xl font-semibold">0{props.item.id}.</p>
             <a href={props.item.github} target="_blank">
                 <img src='images/github-mark-white.png' className='w-[30px] h-[30px]'></img>
             </a>
-            
         </div>
 
         <div className='flex w-full justify-center items-start mb-10 pt-10'>
-            <img src={`images/${props.item.logo}`} className='max-w-[200px] max-h-[100px]'></img>
+            <a className='point-events-auto
+                    sm:pointer-events-none' 
+                href={props.item.link} target="_blank">
+                <img className='max-w-[150px] max-h-[100px]
+                    md:max-w-[200px] md:max-h-[100px]' 
+                    src={`images/${props.item.logo}`}></img>
+            </a>
         </div>
 
-        <div className='flex w-full justify-between px-10'>
-            <p className="text-xl font-semibold">{props.item.title}</p>
-            {props.item.link && <a href={props.item.link} target="_blank">
-                <img src='images/arrow-top-right.png' className='w-[30px] h-[30px]' style={{filter: props.item.id === 1 || props.item.id === 3 ? "" :"invert(100%)"}}></img>
-                </a>}
+        <div className='flex w-full justify-center px-10
+                sm:justify-between'>
+
+            <a className='point-events-auto
+                sm:pointer-events-none' 
+                href={props.item.link} target="_blank">
+                <p className="text-xl font-semibold text-center
+                    sm:text-left">{props.item.title}</p>
+            </a>
+
+            {props.item.link && 
+            <a href={props.item.link} target="_blank">
+                <img className='w-[30px] h-[30px] hidden
+                sm:block' 
+                src='images/arrow-top-right.png' 
+                style={{filter: props.item.id === 1 || props.item.id === 3 ? "" :"invert(100%)"}}></img>
+            </a>}
         </div>
 
     </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,8 @@ import ProjectItem from '../ProjectItem/ProjectItem'
 
 export default function Projects() {
   return (
-    <div className='grid grid-rows-2 grid-cols-2 gap-2 m-2 h-fit' id='landing-link-2'>
+    <div className='grid grid-rows-2 grid-cols-2 gap-2 m-2 h-fit' 
+    id='landing-link-2'>
         <div>
             <ProjectItem item={ProjectsData[0]}></ProjectItem>
         </div>
